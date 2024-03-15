c671e5bcf1c83c0495b990c6b06d37688e37da8c
Meng Chen <mchen24@nd.edu>
2024-03-15 12:48:52 -0400

    netlify config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import DatabaseManager from "./db/database-manager";
+import DatabaseManager from "./db/database-manager.jsx";
 import './main.scss'
 
 // store data into local storage
